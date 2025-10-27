de9856e1245a2b0ec134647903e1c5da26e5f376
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-10-27 23:12:12 +0100

    Refactor layout component structure and add dark mode styles in CSS

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,16 +1,18 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "../components/Navbar"
-import Footer from "../Footer"
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../Footer"; // controlla che il path sia corretto
 
+export default function Layout() {
+  return (
+    <div className="min-h-screen flex flex-col bg-noise-dark text-white">
+      <Navbar />
 
-export default function () {
-    return (
-        <>
-            <Navbar/>
-            <main>
-                <Outlet/>
-            </main>
-            <Footer/>
-        </>
-    )
+      {/* Contenuto principale */}
+      <main className="flex-1  bg-black/60">
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
